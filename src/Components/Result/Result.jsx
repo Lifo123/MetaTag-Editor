9f6443ecec09204b7ded6963c38bdfd9025a77bf
Lifo123 <alberto_p-@hotmail.com>
2024-06-05 10:43:42 -0500

    Add resizable element

--- src/Components/Result/Result.jsx
+++ src/Components/Result/Result.jsx
@@ -3,28 +3,42 @@
 export default function Result() {
     return (
         <section className="f-col result-sec h-100 ">
-            <header className="f-row f-align-center f-justify-between">
-                <div className="f-row g-20 w-100">
-                    <span className="d-flex logo">
-                        <img className="jfN4p" src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png" style={{ background: "none" }} alt="Google" height="30" width="92" />
-                    </span>
-                    <form className="f-row">
-                        <span className="QCzoEc z1asCe MZy1Rb" style={{ height: "20px", lineHeight: "20px", width: "20px" }}>
-                            <svg focusable="false" viewBox="0 0 24 24">
-                                <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-                            </svg>
-                        </span>
-                        <span className="ski"></span>
-                        <span className="ski"></span>
-                        <span className="ski"></span>
-                    </form>
+            <div class="index-preview__slide">
+                <header class="index-preview__header">
+                    <h3>Preview</h3>
+                </header>
+                <div class="index-metadata__content">
+                    <div id="google" class="metadata-group__display is-active">
+                        <h4 class="metadata-group__title"><span>Google</span></h4>
+                        <div class="card-seo-google">
+                            <span class="card-seo-google__title js-preview-title">Manage Tournaments</span>
+                            <div class="card-seo-google__url"><span class="card-seo-google__url-title js-preview-domain">https://lifo123.github.io/ManageTournaments/</span>
+                                <span class="card-seo-google__url-arrow"></span>
+                            </div>
+                            <span class="card-seo-google__description js-preview-description">Manage Tournamentes complety 'FREE' in our website without any cost</span>
+                        </div>
+                    </div>
                 </div>
-                <div className="f-row g-15">
-                    <span className="ski"></span>
-                    <span className="ski"></span>
-                    <span className="ski"></span>
-                </div>
-            </header>
+                <code class="!whitespace-pre hljs language-json">
+                    <span class="hljs-punctuation">{'{'}</span>
+                    <span class="hljs-attr">"scripts"</span>
+                    <span class="hljs-punctuation">:</span>
+                    <span class="hljs-punctuation">{'{'}</span>
+                    <span class="hljs-attr">"build"</span>
+                    <span class="hljs-punctuation">:</span>
+                    <span class="hljs-string">"vite build"</span>
+                    <span class="hljs-punctuation">,</span>
+                    <span class="hljs-attr">"predeploy"</span>
+                    <span class="hljs-punctuation">:</span>
+                    <span class="hljs-string">"npm run build"</span>
+                    <span class="hljs-punctuation">,</span>
+                    <span class="hljs-attr">"deploy"</span>
+                    <span class="hljs-punctuation">:</span>
+                    <span class="hljs-string">"gh-pages -d build"</span>
+                    <span class="hljs-punctuation">{'}'}</span>
+                    <span class="hljs-punctuation">{'}'}</span>
+                </code>
+            </div>
         </section>
     )
 }
--- src/Pages/Index/Index.jsx
+++ src/Pages/Index/Index.jsx
@@ -1,25 +1,127 @@
 import './Styles/Index.css'
-import { useEffect } from 'react'
+import { useContext, useEffect, useRef } from 'react'
+import { GContext } from '../../Context/GlobalContext'
 
 import Coding from '../../Components/Coding/Coding'
 import Result from '../../Components/Result/Result'
 
 
-export default function Index({setProgress}) {
-    
-    //Effects
+export default function Index({ setProgress }) {
+    // Global States
+    const AppContext = useContext(GContext);
+
+    //Refs
+    const Separator = useRef();
+
+    // Functions
+    const HandleMouseDown = (e) => {
+        Separator.current.classList.add('active');
+        document.addEventListener("mousemove", HandleResize);
+        document.addEventListener("mouseup", HandleClear);
+    }
+
+    const HandleResize = (e) => {
+        const leftWidth = Math.max(Math.min(e.clientX, e.view.innerWidth - 1), 3);
+        const rightWidth = e.view.innerWidth - e.clientX;
+
+        Separator.current.previousSibling.style.width = `${leftWidth}px`;
+        Separator.current.nextSibling.style.width = `${rightWidth}px`;
+        Separator.current.style.left = `${leftWidth}px`;
+
+    }
+
+
+    const HandleClear = () => {
+        Separator.current.classList.remove('active');
+        document.removeEventListener("mousemove", HandleResize);
+        document.removeEventListener("mouseup", HandleClear);
+    }
+
+    // Effects
     useEffect(() => {
         setProgress(30);
         setTimeout(() => {
             setProgress(100);
-        }, 200)
-    }, [])
-
+        }, 200);
+    }, [setProgress]);
 
     return (
-        <main className='f-row app-render fixed h-100 w-100'>
-            <Coding />
-            <Result />
+        <main className='app-render fixed h-100 w-100'>
+            <section className='code d-flex relative h-100 pe-1'>
+                <div className='f-col p-2'>
+                    <h3>URL</h3>
+                    <input className='w-60' type="text" placeholder='Search' />
+                    <textarea name=""></textarea>
+                </div>
+            </section>
+
+            <span className='resizer-l absolute' onMouseDown={HandleMouseDown} ref={Separator}></span>
+
+            <section className='view d-grid '>
+                <div className='f-col p-3 g-20'>
+                    <h3>Preview</h3>
+                    <div className="index-metadata__content">
+                        <div className="metadata-group__display is-active">
+                            <h4 className="metadata-group__title"><span>Google</span></h4>
+                            <div className="card-seo-google">
+                                <span className="card-seo-google__title js-preview-title">Manage Tournaments</span>
+                                <div className="card-seo-google__url"><span className="card-seo-google__url-title js-preview-domain">https://lifo123.github.io/ManageTournaments/</span>
+                                    <span className="card-seo-google__url-arrow"></span>
+                                </div>
+                                <span className="card-seo-google__description js-preview-description">Manage Tournamentes complety 'FREE' in our website without any cost</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="index-metadata__content">
+                        <div className="metadata-group__display is-active">
+                            <h4 className="metadata-group__title"><span>Twitter</span></h4>
+                            <div className="card-seo-google">
+                                <span className="card-seo-google__title js-preview-title">Manage Tournaments</span>
+                                <div className="card-seo-google__url"><span className="card-seo-google__url-title js-preview-domain">https://lifo123.github.io/ManageTournaments/</span>
+                                    <span className="card-seo-google__url-arrow"></span>
+                                </div>
+                                <span className="card-seo-google__description js-preview-description">Manage Tournamentes complety 'FREE' in our website without any cost</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="index-metadata__content">
+                        <div className="metadata-group__display is-active">
+                            <h4 className="metadata-group__title"><span>Facebook</span></h4>
+                            <div className="card-seo-google">
+                                <span className="card-seo-google__title js-preview-title">Manage Tournaments</span>
+                                <div className="card-seo-google__url"><span className="card-seo-google__url-title js-preview-domain">https://lifo123.github.io/ManageTournaments/</span>
+                                    <span className="card-seo-google__url-arrow"></span>
+                                </div>
+                                <span className="card-seo-google__description js-preview-description">Manage Tournamentes complety 'FREE' in our website without any cost</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="index-metadata__content">
+                        <div className="metadata-group__display is-active">
+                            <h4 className="metadata-group__title"><span>Linkedin</span></h4>
+                            <div className="card-seo-google">
+                                <span className="card-seo-google__title js-preview-title">Manage Tournaments</span>
+                                <div className="card-seo-google__url"><span className="card-seo-google__url-title js-preview-domain">https://lifo123.github.io/ManageTournaments/</span>
+                                    <span className="card-seo-google__url-arrow"></span>
+                                </div>
+                                <span className="card-seo-google__description js-preview-description">Manage Tournamentes complety 'FREE' in our website without any cost</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="index-metadata__content">
+                        <div className="metadata-group__display is-active">
+                            <h4 className="metadata-group__title"><span>Discord</span></h4>
+                            <div className="card-seo-google">
+                                <span className="card-seo-google__title js-preview-title">Manage Tournaments</span>
+                                <div className="card-seo-google__url"><span className="card-seo-google__url-title js-preview-domain">https://lifo123.github.io/ManageTournaments/</span>
+                                    <span className="card-seo-google__url-arrow"></span>
+                                </div>
+                                <span className="card-seo-google__description js-preview-description">Manage Tournamentes complety 'FREE' in our website without any cost</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
-    )
+    );
 }
